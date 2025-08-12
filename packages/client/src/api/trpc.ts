f8dfae7c2c56a145f0011e83b32a2a1975775571
Neil Huyton <github@nehu.me>
2025-08-12 12:15:08 +0100

    Fix Netlify Functions deployment

--- packages/client/src/api/trpc.ts
+++ packages/client/src/api/trpc.ts
@@ -1,17 +1,13 @@
 import { createTRPCReact } from "@trpc/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { QueryClient } from "@tanstack/react-query";
-import type { AppRouter } from "@my-workspace/server/src/router"; // Import from specific file
+import type { AppRouter } from "@my-workspace/server/src/router";
 
 export const trpc = createTRPCReact<AppRouter>();
-
-export const queryClient = new QueryClient();
 
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      // url: 'http://localhost:3000',
-      url: "/.netlify/functions/api",
+      url: "/.netlify/functions/api/trpc", // Match endpoint in api.ts
     }),
   ],
 });
