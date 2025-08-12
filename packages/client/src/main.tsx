fa35ed1530599398902b7be83ec5ed11abeb8d4a
Neil Huyton <github@nehu.me>
2025-08-12 12:17:28 +0100

    Fix Netlify Functions deployment

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { trpc, queryClient, trpcClient } from "./api/trpc";
-import { QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc, trpcClient } from './api/trpc'; // Remove queryClient import
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './routes/router';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient(); // Create queryClient here
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </trpc.Provider>
   </React.StrictMode>
